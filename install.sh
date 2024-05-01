ce75f0e63d060db0a5ec17a06b14dc644b613fc5
Zic Stardust <git@zicstardust.com>
2024-05-01 06:27:26 -0400

    init commit

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+mkdir -p ${HOME}/.config/systemd/user
+
+cat > ${HOME}/.config/systemd/user/rclone-backup.service << EOF
+[Unit]
+Description=Rclone Backup Service
+Wants=rclone-backup.timer
+
+[Service]
+Type=oneshot
+WorkingDirectory=${HOME}/.config/systemd/
+ExecStart=${HOME}/.config/systemd/rclone-backup.sh
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+
+cat > ${HOME}/.config/systemd/user/rclone-backup.timer << EOF
+[Unit]
+Description=Rclone Backup Timer
+Requires=rclone-backup.service
+
+[Timer]
+Unit=rclone-backup.service
+AccuracySec=1ms
+RandomizedDelaySec=0
+OnCalendar=*-*-* *:00:00
+
+[Install]
+WantedBy=timers.target
+EOF
---
+++ run.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+config_file=${script_dir}/rclone-backup.config
+source $config_file
+
+start(){
+    echo "=============Start rclone backup===================="
+
+    if [ -z ${EXTRAS_ARGS[$i]} ];then
+        FLAGS=""    
+    else    
+        FLAGS="-f ${EXTRAS_ARGS[$i]}" 
+    fi  
+   
+    echo "---BACKUP FROM ${SOURCE_REMOTE[$i]}:${SOURCE_DIR[$i]} TO ${DESTINY_REMOTE[$i]}:${DESTINY_DIR[$i]}---"
+    rclone sync "${SOURCE_REMOTE[$i]}:${SOURCE_DIR[$i]}" "${DESTINY_REMOTE[$i]}:${DESTINY_DIR[$i]}" ${FLAGS} -v          
+    echo "=============End rclone backup===================="
+}
+
+gen-config-file(){
+    if [ -e ${config_file} ];then
+        cat ${config_file} 1> ${config_file}_old
+    fi
+    cat > ${config_file} <<EOF
+SOURCE_REMOTE[0]=
+SOURCE_DIR[0]=
+DESTINY_REMOTE[0]=
+DESTINY_DIR[0]=
+EXTRAS_ARGS[0]=
+
+EOF
+}
+
+check_and_run (){
+    size=${#SOURCE_DIR[@]}
+    for (( i=0; i<${size}; i++)); do
+        start
+    done
+}
+
+if [ "$1" == "gen-config-file" ]; then
+    gen-config-file
+else
+    check_and_run
+fi
